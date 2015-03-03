8992ddd137025a8450c56a24636a14bb90e78104
Harihar Subramanyam <hahasubbu@gmail.com>
2015-03-02 20:38:29 -0500

    Autocomplete textbox

--- static/js/index.js
+++ static/js/index.js
@@ -1,5 +1,15 @@
 (function() {
-  $(document).ready(function() {
+
+  var author_data = null;
+
+  var fetch_author_data = function(callback) {
+    $.get("./data/authors_for_timeline.json", function(data) {
+      author_data = data;
+      callback();
+    });
+  };
+
+  var setup_timeline = function() {
     // DOM element where the Timeline will be attached
     var container = document.getElementById('visualization');
 
@@ -10,19 +20,26 @@
       "selectable": true,
       "align": "left"
     };
-    
-    $.get("./data/authors_for_timeline.json", function(data) {
-      var items = new vis.DataSet(data);
-      var timeline = new vis.Timeline(container, items, options);
-      timeline.on("select", function(selection) {
-        if (selection.items.length > 0) {
-          var id = selection.items[0];
-          var authorName = data[id-1].content;
-          $.get("https://cfrp-api.herokuapp.com/plays?author=eq." + authorName, function(data) {
-            console.log(data);
-          });
-        }
-      });
+    var items = new vis.DataSet(author_data);
+    var timeline = new vis.Timeline(container, items, options);
+    timeline.on("select", function(selection) {
+      if (selection.items.length > 0) {
+        var id = selection.items[0];
+        var authorName = data[id-1].content;
+      }
+    });
+  };
+
+  var setup_typeahead = function() {
+    $("#authorName").typeahead({
+      source: ["alpha", "beta", "gamma"]
+    });
+  };
+
+  $(document).ready(function() {
+    fetch_author_data(function() {
+      setup_timeline();
+      setup_typeahead();
     });
   });
 })();
