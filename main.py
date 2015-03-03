62450328a3f3c4da97c0d82a682f63f9d1914e48
Harihar Subramanyam <hahasubbu@gmail.com>
2015-03-02 21:50:52 -0500

    Getting play data

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import os
 from flask import Flask, url_for, render_template, request, redirect
 import json
+import requests
 
 app = Flask(__name__)
 
@@ -10,6 +11,15 @@ author_dict = {}
 
 for author in author_data:
     author_dict[author["name"]] = author
+
+def get_play_data(name):
+    try:
+        url_path = "http://cfrp-api.herokuapp.com/plays?author=eq." + name
+        r = requests.get(url_path)
+        play_data = json.loads(r.text)
+        return play_data
+    except:
+        return None
 
 # Route for main page. 
 @app.route('/')
@@ -22,7 +32,8 @@ def author():
     if AUTHOR_NAME in request.form:
         author = request.form[AUTHOR_NAME]
         if author in author_dict:
-            return render_template("author.html", author=author_dict[author])
+            play_data = get_play_data(author)
+            return render_template("author.html", author=author_dict[author], play_data=play_data)
     return redirect(url_for("root"))
 
 # Route for static files.
